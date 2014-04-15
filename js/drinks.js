6a9b09dd322595423ff8f58677c215ab39efa840
Walter Menendez <walterm@mit.edu>
2014-04-15 17:14:43 -0400

    Alphabetized drinks

--- js/drinks.js
+++ js/drinks.js
@@ -1,5 +1,7 @@
 var drinks = {
   "Gin and Tonic": "Gin, tonic water",
   "Leap frog": "Gin, ginger ale, lemon juice",
-  "Mimosa": "Champagne, orange juice"
+  "Mimosa": "Champagne, orange juice",
+  "Bloody Mary": "Vodka, tomato juice",
+  "Captain Coke": "Rum, coke"
 };
