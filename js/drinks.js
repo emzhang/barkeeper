3e6014d7c54238fb7fd37056d3c27ed93225af37
Walter Menendez <walterm@mit.edu>
2014-05-02 23:53:04 -0400

    Displays price

--- js/drinks.js
+++ js/drinks.js
@@ -1,8 +1,8 @@
 var drinks = {
-  "Gin and Tonic": "Gin, tonic water",
-  "Leap frog": "Gin, ginger ale, lemon juice",
-  "Mimosa": "Champagne, orange juice",
-  "Bloody Mary": "Vodka, tomato juice",
-  "Captain Coke": "Rum, coke",
-  "Fireball": "Cinnamon whiskey"
+  "Gin and Tonic": ["Gin, tonic water", 4.99],
+  "Leap frog": ["Gin, ginger ale, lemon juice", 4.99],
+  "Mimosa": ["Champagne, orange juice", 4.99],
+  "Bloody Mary": ["Vodka, tomato juice", 4.99],
+  "Captain Coke": ["Rum, coke", 4.99],
+  "Fireball": ["Cinnamon whiskey", 4.99]
 };
