78cd6c683a0ae57fc15fe863eb3983b4e8a02125
Nicholas Paggi <ME@Nicholass-MacBook-Pro.local>
2014-05-08 18:32:10 -0400

    built the basis for persistent data.

--- js/order_queue.js
+++ js/order_queue.js
@@ -1,59 +1,34 @@
-
+      var drinks=drinkList;
+      var loading=true;
       $('#queue').disableSelection();
-      localStorage.setItem("name","Robert Miller")
     	var numdrinks=0;
       var downX=1000000;
       var downY=1000000;
       var startswipe=null;
-    	function addDrink(name, ingredients){
-    		numdrinks+=1;
-			$("#queue").append("<div><div class='row under' id='under"+numdrinks+"'><div class='col-xs-2'><h2>Done</h2></div><div class='col-xs-10'></div></div><div class='row activeDrink' id=drink"+numdrinks+"><div><div class='col-xs-2'><a class='btn-xs' href='./order_queue.html' role='button' id='btn"+numdrinks+"'><img style='width:60px;vertical-align:middle' src='../css/Kitchen-List-ingredients-icon.png'></img></a></div><div class='col-xs-10' align='left'><h2>"+name+"</h2></div></div><div id='collapse"+numdrinks+"' class='panel-collapse collapse in rectangle span12'><div class='panel-body'>"+ingredients+"</div></div></div></div>"
+    	function addDrink(index, name, ingredients, type){
+			$("#queue").append("<div><div class='row under' id='under"+index+"'><div class='col-xs-2'><h2>Done</h2></div><div class='col-xs-10'></div></div><div class='row "+type+"' id=drink"+index+"><div><div class='col-xs-2'><a class='btn-xs' href='./order_queue.html' role='button' id='btn"+index+"'><img style='width:60px;vertical-align:middle' src='../css/Kitchen-List-ingredients-icon.png'></img></a></div><div class='col-xs-10' align='left'><h2>"+name+"</h2></div></div><div id='collapse"+index+"' class='panel-collapse collapse in rectangle span12'><div class='panel-body'>"+ingredients+"</div></div></div></div>"
     		);
-    		var drinkname="#drink"+numdrinks;
-    		var collapsename="#collapse"+numdrinks;
-        var undername="#under"+numdrinks
-    		var spacername="#spacer"+numdrinks;
-        var buttonname="#btn"+numdrinks;
+        var index=index;
+    		var drinkname="#drink"+index;
+    		var collapsename="#collapse"+index;
+        var undername="#under"+index;
+    		var spacername="#spacer"+index;
+        var buttonname="#btn"+index;
     		$(collapsename).toggle();
-    		//$(drinkname).dblclick(function(event){
-    		//	event.preventDefault;
-    		//	$(collapsename).toggle();
-    		//});
     		
-    		//$("#queue").append("<div class='panel-heading span12' id='spacer"+numdrinks+"'></div>");
-        /*$(drinkname).mousedown(function(event){
-          downX=event.clientX;
-          downY=event.clientY;
-          startswipe=this;
-        });
-        $(drinkname).mouseup(function(event){
-          var upX=event.clientX;
-          var upY=event.clientY;
-          console.log(upX-downX);
-          if ((upX-downX>30)&&(startswipe=this)){
-            if ($(drinkname).hasClass("mine")){
-              this.remove();
-             $(spacername).remove();
-           }
-         }
-       });*/
-
-
-
-    	// 	$(drinkname).on("swipe",function(){
-    	// 		 goForIt=false;
-     //       if ($(drinkname).hasClass("mine")){
-     //          this.remove();
-   		// 		   $(spacername).remove();
-     //       }
-  			// });
   			var goForIt=true;
         var kill=false
   			$(drinkname).click(function(event){
     			event.preventDefault;
     			if (goForIt){
             if ($(drinkname).hasClass("activeDrink")&&!$(drinkname).hasClass("notMineDrink")){
+              if (!loading){
+                beingMade[index]=notStarted[index];
+                delete notStarted[index];
+                writeOut();
+              }
     				  $(drinkname).toggleClass('activeDrink mine');
+
               $(drinkname).draggable(
                   {
                     axis: "x",
@@ -61,13 +36,28 @@
                     start: function( event, ui ) {goForIt=false;},
                     drag: function( event, ui ) {if (ui.position.left>95){
                     if ($(drinkname).hasClass("mine")){$(drinkname).toggleClass('mine done').draggable("disable");}}},
-                    stop: function(){if (kill){$(drinkname).remove(); $(undername).remove();}},
+                    stop: function(){if (kill){
+                      $(drinkname).remove();
+                       $(undername).remove();
+                      if (!loading){
+                          if (beingMade[index]!=null){madeDrinks[index]=beingMade[index];}
+                          if (notStarted[index]!=null){madeDrinks[index]=notStarted[index];}
+                          delete beingMade[index];
+                          delete notStarted[index];
+                          writeOut();
+                        }
+                    }},
                     revert: function(event,ui){if ($(drinkname).hasClass("done")){kill=true;}return true;}
                   });
             }
             else if ($(drinkname).hasClass("mine")){
               $(drinkname).toggleClass('activeDrink mine');
               $(drinkname).draggable('destroy');
+              if (!loading){
+                notStarted[index]=beingMade[index];
+                delete beingMade[index];
+                writeOut();
+              }
             }
     			}
     			goForIt=true;
@@ -90,7 +80,6 @@
         sizing(undername,drinkname);
     	}
 
-
     	function notmine(drinkNum){
     		$("#drink"+drinkNum).toggleClass('activeDrink notMineDrink');
     	}
@@ -99,5 +88,12 @@
     		$("#spacer"+drinkNum).remove();
       }
       $(document).ready(function() {
-        jQuery.each( drinks, function( i, val ) {addDrink(i,val);});;
+        jQuery.each( notStarted, function( i, val ) {addDrink(i,val[0],val[1],"activeDrink");});;
     	});
+      $(document).ready(function() {
+        jQuery.each( beingMade, function( i, val ) {
+          addDrink(i,val[0],val[1],"activeDrink");
+          $("#drink"+i).click();
+        });
+        loading=false;
+      });
